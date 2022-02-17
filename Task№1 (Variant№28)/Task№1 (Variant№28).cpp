ad1b08e3cdc173007fd253c8082ed64fbfc70698
Red Star <robertmkrtyan334@gmail.com>
2022-02-17 22:16:05 +0300

    Variant change

---
+++ Task№1 (Variant№28)/Task№1 (Variant№28).cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+const int MAX_LEN = 100;
+
+void AutomaticFilling(int* fillable, int len);
+void ShowArray(int* array, int len);
+
+int main()
+{
+
+	int source[MAX_LEN];
+	int arrayOfPrimes[MAX_LEN];
+
+	AutomaticFilling(source, MAX_LEN);
+	ShowArray(source, MAX_LEN);
+
+}
+
+void AutomaticFilling(int* fillable, int len)
+{
+	srand(time(NULL));
+	for (int i = 0; i < len; i++)
+		fillable[i] = rand() % 100 + 1;
+}
+
+void ShowArray(int* array, int len)
+{
+	printf("[\n");
+	len--;
+
+	for (int i = 0; i < len; i++)
+	{
+		printf("%5i,", array[i]);
+		if ((i > 0) && (i % 10 == 0)) printf("\n");
+	}
+	
+	printf("%5i\n]");
+}
