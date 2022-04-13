9725001ef5e1fdf579ebbee0bd3df19f324eb14b
RedStar <robertmkrtyan334@gmail.com>
2022-04-13 11:49:29 +0300

    .

--- №5/SinglyLinkedList/List.h
+++ №5/SinglyLinkedList/List.h
@@ -7,13 +7,18 @@ class List
 {
 	public:
 		List();
+		List(const List<T>& other);
 		~List();
 
 		unsigned GetSize() { return size; }
 		void Push_back(T data);
+		void DeleteAt(unsigned index);
+		void Clear();
+
+		T& operator[](unsigned index);
 
 	private:
-		template<typename T>
+		template<typename>
 		struct Node
 		{
 				Node* pNext;
@@ -29,6 +34,9 @@ class List
 		void CreateHead(T data);
 		void CreateNewNode(T data);
 
+		void DeleteHead();
+		void DeleteNode(unsigned index);
+
 		unsigned size;
 		Node<T> *head;
 };
@@ -38,10 +46,26 @@ List<T>::List()
 {
 	size = 0;
 	head = nullptr;
-};
+}
 template<typename T>
-inline List<T>::~List()
+List<T>::List(const List<T>& other)
 {
+	size = other.size;
+	this->head = new Node<T>(other.head->data);
+
+	Node<T>* currentNode = this->head;
+	Node<T>* otherCurrentNode = other.head->pNext;
+	while (otherCurrentNode != nullptr)
+	{
+		currentNode->pNext = new Node<T>(otherCurrentNode->data);
+		currentNode = currentNode->pNext;
+		otherCurrentNode = otherCurrentNode->pNext;
+	}
+}
+template<typename T>
+List<T>::~List()
+{
+	Clear();
 }
 
 template<typename T>
@@ -71,4 +95,67 @@ void List<T>::CreateNewNode(T data)
 
 	currentNode->pNext = new Node<T>(data);
 	size++;
+}
+
+template<typename T>
+void List<T>::Clear()
+{
+	while (size)
+		DeleteHead();
+}
+template<typename T>
+void List<T>::DeleteAt(unsigned index)
+{
+	unsigned currentIndex = 0;
+	Node<T>* currentNode = head;
+	if (index == 0)
+	{
+		DeleteHead();
+	}
+	else
+	{
+		DeleteNode(index);
+	}
+}
+template<typename T>
+void List<T>::DeleteHead()
+{
+	Node<T>* temp = head;
+	head = head->pNext;
+	delete temp;
+
+	size--;
+}
+template<typename T>
+void List<T>::DeleteNode(unsigned index)
+{
+	Node<T>* previous = head;
+	for (unsigned i = 0; i < index - 1; i++)
+		previous = previous->pNext;
+	
+	Node<T>* toDelete = previous->pNext;
+	previous->pNext = toDelete->pNext;
+	delete toDelete;
+
+	size--;
+}
+
+template<typename T>
+T& List<T>::operator[](unsigned index)
+{
+	unsigned currentIndex = 0;
+	T none = T();
+	Node<T>* currentNode = head;
+
+	while (currentNode != nullptr)
+	{
+		if (currentIndex == index)
+		{
+			return currentNode->data;
+		}
+		currentNode = currentNode->pNext;
+		currentIndex++;
+	}
+
+	return none;
 }
