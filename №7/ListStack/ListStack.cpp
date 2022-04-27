ebedcf5742a0a71648e39b1069b464567e4c5789
RedStar <robertmkrtyan334@gmail.com>
2022-04-27 18:21:23 +0300

    .

--- №7/ListStack/ListStack.cpp
+++ №7/ListStack/ListStack.cpp
@@ -1,26 +1,70 @@
 ﻿#include <iostream>
+#include <string>
+#include <map>
 #include "ListStack.h"
 
 using namespace std;
+
+map<char, unsigned short> prec ={	{'/', 3},
+									{'*', 3},
+									{'+', 2},
+									{'-', 2},
+									{'(', 1}
+								};
+
+string infixToPostfix(string infix);
 
 int main()
 {
 	setlocale(LC_ALL, "ru");
 
-	ListStack<int> stack;
-
-	stack.Push(97);
-	stack.Push(34);
-	stack.Push(10);
-	stack.Push(29);
-	stack.Push(1);
-	stack.Push(53);
-	stack.Push(3);
-	stack.Push(2);
-
-	int var;
-	while (stack.Pop(var))
-		printf("%i\n", var);
+	printf("%s", infixToPostfix("( A + B ) * ( C + D )").c_str());
 
 	return 0;
+}
+
+string infixToPostfix(string infix)
+{
+	ListStack<char> opStack;
+	string postfix;
+
+	for (int i = 0; i < infix.size(); i++)
+	{
+		if ((47 < infix[i] < 58) or (64 < infix[i] < 91))
+			postfix += infix[i] + ' ';
+		else if (infix[i] == '(')
+			opStack.Push(infix[i]);
+		else if (infix[i] == ')')
+		{
+			char topChar;
+			opStack.Pop(topChar);
+			while (topChar != '(')
+			{
+				postfix += topChar;
+				opStack.Pop(topChar);
+			}
+		}
+		else
+		{
+			char currentChar;
+			opStack.GetHead(currentChar);
+			while (!opStack.IsEmpty() and (prec.at(currentChar) >= prec.at(infix[i])))
+			{
+				opStack.Pop(currentChar);
+				postfix += currentChar + ' ';
+			}
+
+			opStack.Push(infix[i]);
+		}
+	}
+
+	while (!opStack.IsEmpty())
+	{
+		char currentChar;
+		opStack.Pop(currentChar);
+
+		postfix += currentChar + ' ';
+	}
+
+	return postfix;
 }
